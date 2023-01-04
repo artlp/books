716415a43d550098358ace68a4a0362924b4f8e8
Artur Lipskii <falcon.arthur@gmail.com>
2023-01-04 18:39:35 +0200

    some work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import BookCreate from "./components/BookCreate.jsx";
+
 function App() {
-  return <div className="App">
-    App
-  </div>;
+  const [books, setBooks] = useState([]);
+  const createBook = (title) => {
+    
+  };
+
+  return (
+    <div className="App">
+      <BookCreate onCreate={createBook} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/BookCreate.jsx
+++ src/components/BookCreate.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { useState } from "react";
 
-function BookCreate() {
-  return <div>BookCreate</div>;
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState("");
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle("");
+  };
+  return (
+    <div className="book-create">
+      <h3>Add a book</h3>
+      <form onSubmit={handleSubmit}>
+        <label>Title</label>
+        <input
+          type="text"
+          name=""
+          id=""
+          className="input"
+          value={title}
+          placeholder="Add a book"
+          onChange={handleChange}
+        />
+        <button className="button">Create</button>
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
