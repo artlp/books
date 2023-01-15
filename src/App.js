e5181ab7cf329bc34278cd7ec67133d8ad05ecbd
Artur Lipskii <falcon.arthur@gmail.com>
2023-01-15 21:14:28 +0200

    aa

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import BookList from "./components/BookList.jsx";
 import BooksContext from "./context/books.js";
 
 function App() {
-  const { fetchBooks, books } = useContext(BooksContext);
+  const { fetchBooks } = useContext(BooksContext);
   useEffect(() => {
     fetchBooks();
   }, []);
-  
+
   return (
     <div className="app">
       <h1>Reading list</h1>
